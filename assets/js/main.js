155145a08c0d8320c005233d18a5be0be7bab907
Luigi Rocha <gio030@email.com>
2026-04-22 22:08:57 +0100

    modified main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
-// Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('myButton');
+const button = document.getElementById('myButton');
 
-    button.addEventListener('click', () => {
-        alert('Button functionality is working!');
-    });
+button.addEventListener('click', () => {
+    alert('Button functionality is working!');
 });
